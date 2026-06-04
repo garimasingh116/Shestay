c814ea763f62d3c84c2860e7046edd5a7dc3eae1
garimasingh116 <garimasingh0316@gmail.com>
2026-06-04 13:47:22 +0530

    dockerized shestay

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ const { createClient } = require("redis");
 
 const redisClient = createClient({
   url: process.env.REDIS_URL ||
-       "redis://localhost:6379"
+       "redis://redis:6379"
 });
 
 redisClient.on("error", (err) => {
